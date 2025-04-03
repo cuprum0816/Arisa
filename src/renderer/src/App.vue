0e7fed62a2ae944f1d02201e83b9702b30ef02a5
Cuprum <438171310@qq.com>
2025-04-03 19:52:49 +0800

    Created Navigation Bar

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,13 @@
-<script setup lang="ts">
-import Versions from './components/Versions.vue'
+<!--
+  App.vue
+  该文件旨在作为应用程序的入口点，主要用于引入和渲染其他组件。
+-->
 
-const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+<script setup lang="ts">
+import navigationBar from './components/navigation-bar/navigation-bar.vue'
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
+  <navigationBar />
+
 </template>
---
+++ src/renderer/src/components/navigation-bar/navigation-bar.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <div class="logo">
+      <img class="logo" src="../../assets/icon.png" />
+    </div>
+    <div class="greeting">
+      <el-text :style="{ fontSize: 'large', fontWeight: 'bold' }">Arisa</el-text>
+    </div>
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">样本清洗</el-menu-item>
+    <el-menu-item index="3">样本可视化</el-menu-item>
+    <el-menu-item index="3">帮助文档</el-menu-item>
+    <el-menu-item index="4">设置</el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ElMenu, ElMenuItem, ElSubMenu } from 'element-plus'
+import { ref } from 'vue'
+
+const activeIndex = ref('1') // Define the activeIndex property
+
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<style scoped>
+.logo {
+  width: 40px;
+  height: 40px;
+  margin-right: 30px;
+  margin-left: 10px;
+  margin-top: 5px;
+}
+.greeting {
+  display: flex;
+  font-size: 40px;
+  margin-right: 30px;
+}
+</style>
